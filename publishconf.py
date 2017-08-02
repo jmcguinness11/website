8641d88ba224be7f5628ee7891113137ae8b80f9
John McGuinness <johnmcguinness@Johns-MacBook-Pro.local>
2017-08-02 21:23:25 +0200

    fixed error

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://jmcguinness11.github.io'
+SITEURL = 'https://jmcguinness11.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
